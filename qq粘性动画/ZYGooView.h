cebc03a7cb13af7ff99a65209d9fc0e2ff22a338
wzpziyi1 <1204473090@qq.com>
2016-02-17 16:40:56 +0800

    first commit

---
+++ qq粘性动画/ZYGooView.h
@@ -0,0 +1,13 @@
+//
+//  ZYGooView.h
+//  qq粘性动画
+//
+//  Created by 王志盼 on 16/2/17.
+//  Copyright © 2016年 王志盼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZYGooView : UIButton
+
+@end
---
+++ qq粘性动画/ZYGooView.m
@@ -0,0 +1,21 @@
+//
+//  ZYGooView.m
+//  qq粘性动画
+//
+//  Created by 王志盼 on 16/2/17.
+//  Copyright © 2016年 王志盼. All rights reserved.
+//
+
+#import "ZYGooView.h"
+
+@implementation ZYGooView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
