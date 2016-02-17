9b8ce9bd284bff0e688ffe0b534ea0212fb4bd03
wzpziyi1 <1204473090@qq.com>
2016-02-17 13:14:35 +0800

    Initial Commit

---
+++ qq粘性动画/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  qq粘性动画
+//
+//  Created by 王志盼 on 16/2/17.
+//  Copyright © 2016年 王志盼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
